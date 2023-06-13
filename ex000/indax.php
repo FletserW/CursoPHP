3d656ec716baea09f3ecb3ffa0f42799abec81a1
FletserW <130801024+FletserW@users.noreply.github.com>
2023-06-13 12:26:54 -0300

    create ex0,1,2

---
+++ ex000/indax.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ritual</title>
+</head>
+<body>
+    <h1>
+        <?php 
+            print "Ola Mundo \u{1F30E}";
+        ?>
+    </h1>
+    <p>Vamos no livrar da maldição.</p>
+</body>
+</html>
